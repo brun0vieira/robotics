40d4b78e5b73bd15a162233bb7b7a431da7f87c8
brun0vieira <brunoarvieira@gmail.com>
2021-05-15 10:35:33 +0100

    [inverse_kinematics] pose to matrix done

--- lab2/catkin_ws/src/tr5_kinematics_source_code/tr5_kinematics/src/tr5_inverse_kin_node.py
+++ lab2/catkin_ws/src/tr5_kinematics_source_code/tr5_kinematics/src/tr5_inverse_kin_node.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python  
 import rospy
 import math
-"""
-TODO add any missing imports (e.g. custom messages/services)
-"""
+
+import tf
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Pose
+from tr5_kinematics.srv import DoInverseKinematics, DoInverseKinematicsResponse
 
 # load private parameters
 svc_ik = rospy.get_param("~inv_kin_service", "/do_ik")     
@@ -14,17 +16,73 @@ a2 = 0.200
 a3 = 0.130
 d5 = 0.130
 
-"""
-TODO Implement the callback to handle the inverse kinematics calculations 
-"""
+# receives a pose and returns the matrix entries
+def poseToMatrix(pose):
+    position = []
+    orientation = []
+    
+    # translation expressed as a tuple (x,y,z)
+    position = position.append(pose.position.x)
+    position = position.append(pose.position.y)
+    position = position.append(pose.position.z)
+
+    # rotation quaternion expressed as a tuple (x,y,z,w)
+    orientation = orientation.append(pose.orientation.x)
+    orientation = orientation.append(pose.orientation.y)
+    orientation = orientation.append(pose.orientation.z)
+    orientation = orientation.append(pose.orientation.w)
+
+    # converts a tf into a 4x4 matrix
+    # |nx sx ax px|
+    # |ny sy ay py|
+    # |nz sz az pz|
+    # | 0  0  0  1|
+    m = tf.TransformerROS().fromTranslationRotation(position, orientation)
+
+    nx = m[0][0]
+    ny = m[1][0]
+    nz = m[2][0]
+    sx = m[0][1]
+    sy = m[1][1]
+    sz = m[2][1]
+    ax = m[0][2]
+    ay = m[1][2]
+    az = m[2][2]
+    px = m[0][3]
+    py = m[1][3]
+    pz = m[2][3]
+
+    return (nx,ny,nz,sx,sy,sz,ax,ay,az,px,py,pz)
+
+def arctg2(y,x):
+    return 0 if y==0 and x==0 else math.atan2(y,x)
+
+def sqrt(x):
+    return 0 if x < 0 else math.sqrt(x)
+
+# callback to handle the inverse kinematics calculations
 def doInverseKinematics(srv):
-    return 
+    response = JointState()
+
+    # converts tf into a matrix and get its entries
+    (nx,ny,nz,sx,sy,sz,ax,ay,az,px,py,pz) = poseToMatrix(srv.req_pose)
+
+    theta1 = arctg2(py,px) 
+    theta234 = arctg2(az, ax*math.cos(theta1) + ay*math.sin(theta1)) 
+    c_theta3 = (((px*math.cos(theta1) + py*math.sin(theta1) - d5*math.cos(theta234))**2) + ((pz-d1-d5*math.sin(theta234))**2) - a2**2 - a3**2) / (2*a2*a3)
+    s_theta3 = sqrt(1-c_theta3**2)
+    theta3 = math.atan(sqrt(1/(c_theta3**2)-1))
+    theta2 = arctg2(((a2 + a3*math.cos(theta3))*(-d1+pz-d5*math.sin(theta234))), 
+                    ((a2+a3*math.cos(theta3))*(-d5*math.cos(theta234) + math.cos(theta1)*px + math.sin(theta1)*py) + a3*math.sin(theta3)*(-d1+pz-d5*math.sin(theta234))))
+    theta4 = theta234 - theta2 - theta3
+    theta5 = math.atan((-ny*math.cos(theta1) + nx*math.sin(theta1)) / (-sy*math.cos(theta1) + sx*math.sin(theta1)))
+
+    return DoInverseKinematicsResponse(response)
 
 if __name__ == "__main__":
     rospy.init_node('tr5_inverse_kinematics')
-    """
-    TODO Create and advertise the service for inverse kinematics
-    """
+    # create and advertise the service for inverse kinematics
+    do_ik = rospy.Service(svc_ik, DoInverseKinematics, doInverseKinematics)
 
     while not rospy.is_shutdown():
         rospy.spin()
